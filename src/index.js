94424e71bbd330c7f50a88740b84a35849438a2c
Arvind Kalra <kalarvind97@gmail.com>
2022-12-31 17:06:08 +0530

    Removed the old transaction popup page from the code

--- src/index.js
+++ src/index.js
@@ -1,68 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {
-    createBrowserRouter,
-    RouterProvider
-} from "react-router-dom";
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import CreateWallet from "./pages/CreateWallet";
 import ImportWalletPage from "./pages/ImportWallet";
 import SetupWallet from "./pages/SetupWallet";
 import NewRequestPage from "./components/NewRequestPage";
 import QRCodeScanner from "./pages/QRCodeScanner";
 import ChatPage from "./pages/ChatPage";
-import TransactionPopup from "./pages/TransactionPopup";
-import NewSendMoneyPage from './components/NewSendMoneyPage';
-import {subscribeToWebNotifications} from "./helpers";
+import NewSendMoneyPage from "./components/NewSendMoneyPage";
 
-const router = createBrowserRouter(
-    [
-        {
-            path: "/",
-            element: <App />
-        },
-        {
-            path: "/wallet/new",
-            element: <SetupWallet />
-        },
-        {
-            path: "/wallet/create",
-            element: <CreateWallet />
-        },
-        {
-            path: "/wallet/import",
-            element: <ImportWalletPage />
-        },
-        {
-            path: "/request/new",
-            element: <NewRequestPage />
-        },
-        {
-            path: "/send/new",
-            element: <NewSendMoneyPage />
-        },
-        {
-            path: "/scan",
-            element: <QRCodeScanner />
-        },
-        {
-            path: "/chat",
-            element: <ChatPage />
-        },
-        {
-            path: "/transaction",
-            element: <TransactionPopup />
-        }
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/wallet/new",
+    element: <SetupWallet />,
+  },
+  {
+    path: "/wallet/create",
+    element: <CreateWallet />,
+  },
+  {
+    path: "/wallet/import",
+    element: <ImportWalletPage />,
+  },
+  {
+    path: "/request/new",
+    element: <NewRequestPage />,
+  },
+  {
+    path: "/send/new",
+    element: <NewSendMoneyPage />,
+  },
+  {
+    path: "/scan",
+    element: <QRCodeScanner />,
+  },
+  {
+    path: "/chat",
+    element: <ChatPage />,
+  },
+]);
 
-    ]
-);
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <RouterProvider router={router} />
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
