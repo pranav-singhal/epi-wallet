dc9986d5df39b8fbd674c04af70dfd7d8698a808
Arvind Kalra <kalarvind97@gmail.com>
2023-01-14 02:57:17 +0530

    Changed the title of chains, and updated the styles of chain switcher a bit

--- src/components/ChainSwitcher/index.js
+++ src/components/ChainSwitcher/index.js
@@ -1,40 +1,39 @@
 import React, { useContext } from "react";
 import { DownOutlined } from "@ant-design/icons";
 import { Dropdown } from "antd";
-import _ from 'lodash';
+import _ from "lodash";
 import chainList from "../../helpers/chains.json";
 import { ChainContext } from "../..";
 
 const ChainSwitcher = () => {
-
-    const [chainState, dispatch] = useContext(ChainContext);
-    return (
-        <Dropdown menu={{items: _.map(chainList, (chainItem => {
-            return {
-              key: chainItem.name,
-              label: chainItem.title,
-              icon: chainItem.icon
-            }
-          })),
-          onClick: (selectedItem) => {
-            const selectedChain = _.filter(chainList, chainItem => (chainItem.name === selectedItem.key))[0];
-            dispatch({
-                    type: "switch_chain",
-                    chain: selectedChain?.name 
-            })
-          }
-        }}
-          >
-            <div className="chain-switcher">
-              <span>
-                  {
-                      chainState.title || chainState.name
-                  }
-                </span>
-              <DownOutlined />
-            </div>
-            </Dropdown>
-    )
-}
+  const [chainState, dispatch] = useContext(ChainContext);
+  return (
+    <Dropdown
+      menu={{
+        items: _.map(chainList, (chainItem) => {
+          return {
+            key: chainItem.name,
+            label: chainItem.title,
+          };
+        }),
+        onClick: (selectedItem) => {
+          const selectedChain = _.filter(
+            chainList,
+            (chainItem) => chainItem.name === selectedItem.key
+          )[0];
+          dispatch({
+            type: "switch_chain",
+            chain: selectedChain?.name,
+          });
+        }
+      }}
+    >
+      <div className="chain-switcher">
+        <span>{chainState.title || chainState.name}</span>
+        <DownOutlined />
+      </div>
+    </Dropdown>
+  );
+};
 
 export default ChainSwitcher;
